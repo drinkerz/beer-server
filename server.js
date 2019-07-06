f2faf771544bbf1bef3fbaefd5c6e6e6a183c578
moong <moong@fltgraph.com>
2019-07-06 19:39:45 +0900

    Add beer data

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const app = express();
 const port = process.env.PORT || 40001;
 
 const mockBeer = require('./mockBeer.json');
-const mockReview = require('./mockReview.json');
 
 app.use(cors());
 app.use(logger('tiny'));
@@ -19,8 +18,17 @@ app.get('/api/v1/beers', (req, res) => {
   res.send(mockBeer);
 });
 
+app.get('/api/v1/beer/:name', (req, res) => {
+  const name = req.params.name.replace(/-/g, ' ');
+  const beer = mockBeer.filter(beer => beer.name.toLowerCase() === name)[0];
+  res.send(beer);
+})
+
 app.get('/api/v1/reviews', (req, res) => {
-  res.send(mockReview);
+  const reviews = mockBeer.reduce((arr, beer) => {
+    return [...arr, ...beer.reviews];
+  }, []);
+  res.send(reviews);
 });
 
 app.listen(port, (err) => {
